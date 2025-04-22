2ee09d5da68d843c034a8731d28652eacf67e8ca
Samuel <samuel05.hb@gmail.com>
2025-04-22 18:29:57 -0400

    Fixed the issue where my Finnhub API Key was not being parsed correctly within the url string template literal.

--- src/hooks/useFinnhubData.tsx
+++ src/hooks/useFinnhubData.tsx
@@ -1,19 +1,8 @@
 'use client'; 
 import { useQuery } from '@tanstack/react-query';
 
-// const finnhub_api_key = process.env.REACT_APP_FINNHUB_API_KEY;
-
-const url = `https://finnhub.io/api/v1/news?category=general&token=cvnh48hr01qq3c7fa2vgcvnh48hr01qq3c7fa300`;
-
-// interface FinnhubPost {
-//     id: number,
-//     category: string,
-//     headline: string, 
-//     image: string,
-//     source: string,
-//     summary: string,
-//     url: string,
-// }
+const finnhub_api_key = process.env.NEXT_PUBLIC_FINNHUB_API_KEY;
+const url = `https://finnhub.io/api/v1/news?category=general&token=${finnhub_api_key}`;
 
 const fetchFinnhubData = async() => {
     const response = await fetch(url);
@@ -23,29 +12,11 @@ const fetchFinnhubData = async() => {
 }
 
 const useFinnhubData = () => {
-
     return useQuery({
         queryKey: ['finnhub-news'],
         queryFn: fetchFinnhubData,
         staleTime: 1000 * 60 * 5 // 5 minutes cache
     })
-
-    // const {data, isPending, isError, error} = useQuery({
-    //     queryKey: ['finnhubData'],
-    //     queryFn: fetchFinnhubData,
-    // })
-
-    // if(isPending) { return <div>Loading...</div> }
-    // if(isError) { return <div>Error: {error.message}</div> }
-
-    // return(
-    //     <div>
-    //         {data.map((post: FinnhubPost) => {
-    //             return <div key={post.id}>{post.headline}</div>;
-    //         })}
-    //     </div>
-    // )
 };
 
 export default useFinnhubData;
-// export default fetchFinnhubData;
