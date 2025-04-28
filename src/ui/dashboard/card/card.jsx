add9e7068ee5f2ae0b72edaaff6e12deaf469808
Samuel Hernandez <samuelhernandez@Mac.lan>
2025-04-27 22:55:22 -0400

    Decided to add hardcoded data into the cards rather than displaying API data. (Will display user data later on.)

--- src/ui/dashboard/card/card.jsx
+++ src/ui/dashboard/card/card.jsx
@@ -1,71 +1,44 @@
-'use client'; 
-
-import { FaApple } from "react-icons/fa";
-import { BsAmazon, BsNvidia } from "react-icons/bs";
+import { FaPiggyBank, FaMoneyBillWave } from "react-icons/fa";
+import { FaUserTie } from "react-icons/fa6";
 import styles from "./card.module.css";
-import useFinnhubStocks from "@/hooks/useFinnhubStocks"
-
-const stockSymbols = [
-  {symbol : 'AAPL', Icon : FaApple},
-  {symbol : 'AAPL', Icon : BsAmazon},
-  {symbol : 'AAPL', Icon : BsNvidia}
-]
 
 const Card = () => {
-
-  const symbols = stockSymbols.map((sym) => { sym.symbol });
-  const {data, isPending, isError, error} = useFinnhubStocks(symbols);
-  if(isPending) { return <div>Loading...</div> };
-  if(isError) { return <div>Error: {error.message}</div> };
-  console.log(data);
-
-  return (
+    return (
+      <>
       <div className={styles.container}>
-        <FaApple size={24} />
+        <FaUserTie size={24} />
         <div className={styles.texts}>
-          <span className={styles.title}>Apple Inc</span>
-          <span className={styles.number}>10,273 Users</span>
+          <span className={styles.title}>Total Portfolio Value</span>
+          <span className={styles.number}>$63,097</span>
           <span className={styles.detail}>
-            <span className={styles.positive}>34%</span> more than previous week
+            <span className={styles.positive}>23.12%</span> more than previous week
           </span>
         </div>
       </div>
-      
-    // <>
-    // <div className={styles.container}>
-    //   <FaApple size={24} />
-    //   <div className={styles.texts}>
-    //     <span className={styles.title}>Apple Inc</span>
-    //     <span className={styles.number}>10,273 Users</span>
-    //     <span className={styles.detail}>
-    //       <span className={styles.positive}>34%</span> more than previous week
-    //     </span>
-    //   </div>
-    // </div>
 
-    // <div className={styles.container}>
-    //   <BsAmazon size={24} />
-    //   <div className={styles.texts}>
-    //     <span className={styles.title}>Amazon Inc</span>
-    //     <span className={styles.number}>10,273 Users</span>
-    //     <span className={styles.detail}>
-    //       <span className={styles.positive}>34%</span> more than previous week
-    //     </span>
-    //   </div>
-    // </div>
+      <div className={styles.container}>
+        <FaPiggyBank size={24} />
+        <div className={styles.texts}>
+          <span className={styles.title}>Available to Invest</span>
+          <span className={styles.number}>$24,120</span>
+          <span className={styles.detail}>
+            <span className={styles.positive}>11.98%</span> more than previous week
+          </span>
+        </div>
+      </div>
 
-    // <div className={styles.container}>
-    //   <BsNvidia size={24} />
-    //   <div className={styles.texts}>
-    //     <span className={styles.title}>Nvidia</span>
-    //     <span className={styles.number}>10,273 Users</span>
-    //     <span className={styles.detail}>
-    //       <span className={styles.positive}>34%</span> more than previous week
-    //     </span>
-    //   </div>
-    // </div>
-    // </>
-  );
+      <div className={styles.container}>
+        <FaMoneyBillWave size={24} />
+        <div className={styles.texts}>
+          <span className={styles.title}>Top Gainer</span>
+          <span className={styles.number}>AAPL</span>
+          <span className={styles.detail}>
+            <span className={styles.positive}>4.58%</span> more than previous week
+          </span>
+        </div>
+      </div>
+      </>
+    );
 };
 
 export default Card;
