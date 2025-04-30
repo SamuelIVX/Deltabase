5cf5a7e9387f6079df9d85af8f6e35bab2f593c2
Samuel Hernandez <samuelhernandez@Mac.lan>
2025-04-29 22:07:12 -0400

    Added a login page that takes a username and password, redirecting them to the dashboard. (Which will be personalized and distinct based on each user later on.)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,22 @@
+"use client"
 import styles from "@/ui/page/page.module.css";
+import { useRouter } from "next/navigation";
 
 const LoginPage = () => {
+
+  const router = useRouter();
+  const handleSumbit = async(event: React.FormEvent<HTMLFormElement>) =>{
+    event.preventDefault();
+    router.push("/dashboard");
+  }
+
   return (
     <div className={styles.container}>
-      <form className={styles.form}>
+      <form className={styles.form} onSubmit={ handleSumbit }>
         <h1>Login</h1>
         <input type="text" placeholder="username" name="username" />
         <input type="password" placeholder="password" name="password" />
-        <button>Login</button>
+        <button type="submit">Login</button>
       </form>
     </div>
   );
