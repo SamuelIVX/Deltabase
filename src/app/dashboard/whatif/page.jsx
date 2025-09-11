552f3db97c3aa5c24eb5b00d1e82bd7667b4a785
Samuel <samuel05.hb@gmail.com>
2025-09-11 14:44:18 -0400

    Added the investment form component. Used 'radix-ui/react-slider' to implement a slider. This is so the user can choose how long they want to invest in the given stocks. Added two input components where the user will have to give in an initial/monthly investment for the given stocks.

--- src/app/dashboard/whatif/page.jsx
+++ src/app/dashboard/whatif/page.jsx
@@ -1,11 +1,13 @@
-import AssetSelector from "@/components/whatif/assetselector/assetselector";
 import Blurb from "@/components/whatif/blurb/blurb";
+import AssetSelector from "@/components/whatif/assetselector/assetselector";
+import InvestmentForm from "@/components/whatif/investmentform/investmentform";
 
 const WhatIf = () => {
     return (
         <div>
             <Blurb />
             <AssetSelector />
+            <InvestmentForm />
         </div>
     )
 }
---
+++ src/components/whatif/investmentform/investmentform.jsx
@@ -0,0 +1,90 @@
+'use client'
+import useFinnhubHistoricalData from "@/hooks/useFinnhubHistoricalData"
+import styles from "./investmentform.module.css";
+import * as Slider from "@radix-ui/react-slider"
+import { useState } from "react"
+
+function TimeSlider() {
+    const [value, setValue] = useState([1]); 
+  
+    const years = value[0];
+    const label = years === 1 ? 'Year' : 'Years';
+  
+    return (
+      <>
+        <Slider.Root
+          value={value}
+          onValueChange={setValue}
+          min={1}
+          max={10}
+          step={1}
+          className={styles.SliderRoot}
+        >
+          <Slider.Track className={styles.SliderTrack}>
+            <Slider.Range className={styles.SliderRange} />
+          </Slider.Track>
+          <Slider.Thumb className={styles.SliderThumb} aria-label="Years" />
+        </Slider.Root>
+  
+        <p className={styles.label}> {years} {label}</p>
+      </>
+    );
+  }
+
+const InvestmentForm = () => {
+
+    const [initialInvestment, setInitialInvestment] = useState("");
+    const [monthlyInvestment, setMonthlyInvestment] = useState("");
+
+    const handleInitial = (e) => {
+        setInitialInvestment(e.target.value);
+    }
+
+    const handleMonthly = (e) => {
+        setMonthlyInvestment(e.target.value);
+    }
+
+    return (
+        <>
+        <div className={styles.wrapper}>
+            <div className={styles.container}>
+
+                <div className={styles.field}>
+                    <label className={styles.label}>Initial Investment:</label>
+                        <div className={styles.inputBg}>
+                            <input
+                            className={styles.input}
+                            type="text"
+                            value={initialInvestment}
+                            onChange={handleInitial}
+                            placeholder="Value here..."
+                            />
+                        </div>
+                </div>
+
+                <div className={styles.field}>
+                    <label className={styles.label}>Monthly Investment:</label>
+                    <div className={styles.inputBg}>
+                        <input
+                        className={styles.input}
+                        type="text"
+                        value={monthlyInvestment}
+                        onChange={handleMonthly}
+                        placeholder="Value here..."
+                        />
+                    </div>
+                </div>
+
+            </div>
+
+            <div className={styles.slider}>
+                <label className={styles.sliderLabel}>How long do you plan to invest in these stocks?</label>
+                <TimeSlider />
+            </div>
+
+        </div>
+        </>
+    )
+}
+
+export default InvestmentForm;
