7e9c0a6a5660f36d9a7532299a98911f645e1772
Samuel <samuel05.hb@gmail.com>
2025-10-11 22:20:24 -0400

    Reverting previous changes.

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
-  trailingSlash: true,
-  images: {
-    unoptimized: true
-  },
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/deltabase' : '',
-  basePath: process.env.NODE_ENV === 'production' ? '/deltabase' : '',
+  /* config options here */
 };
 
 export default nextConfig;
